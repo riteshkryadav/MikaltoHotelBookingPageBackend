210a6a5074b76f7db9a617d60eca0422d711f4f1
Ritesh Kumar Yadav <ritesh@itobuz.com>
2023-03-29 18:37:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const port = 8080;
+const path = require("path");
+const fs = require("fs/promises");
+const fsx = require("fs");
+
+const filePath = path.join(__dirname + "/data.json");
+const data = JSON.parse(fsx.readFileSync(filePath, "utf-8"));
+
+http.createServer((req, res) => {
+    try {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    const formData = [];
+    req.on("data", (formDataPieces) => {
+      formData.push(formDataPieces);
+    });
+    req.on("end", () => {
+      console.log(Buffer.concat(formData).toString());
+    
+      let formdata = Buffer.concat(formData).toString();
+
+      let getData = async() => {
+        console.log(formdata);
+        console.log("hi")
+        let presentData = await fs.writeFile("./data.txt", formData);
+        formDataNew = formData + ";" + presentData;
+        await fs.writeFile("./data.txt", formDataNew);
+      }
+getData();
+    })
+    //   res.write(JSON.stringify(data));
+      res.end(JSON.stringify(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }).listen(port, () => {
+    console.log(`Listening to port ${port} `);
+  });
